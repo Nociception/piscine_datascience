5f7cfc1d01e97d1e0f603a6e4011719c6b48011d
Nicolas Stoutzer <nstoutze@42nice.fr>
2025-01-15 14:37:03 +0100

    day00/ex02: DONE, understood and explain for good this time

--- Day00-creation_of_a_db/ex02/table.sql
+++ Day00-creation_of_a_db/ex02/table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS data_2022_dec (
-    event_time TIMESTAMP,
+    event_time TIMESTAMP WITH TIME ZONE,
     event_type VARCHAR(50),
     product_id INT,
     price NUMERIC(10, 2),
     user_id BIGINT,
-    user_session VARCHAR(50)
+    user_session UUID
 );
 
 COPY data_2022_dec (event_time, event_type, product_id, price, user_id, user_session)
