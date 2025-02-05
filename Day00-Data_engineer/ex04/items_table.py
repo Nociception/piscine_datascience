8dbe4ab200ef35dca80ff2c9322267c6a6fd46ad
Nociception <nicolas.stoutzer@hotmail.fr>
2025-02-05 17:32:56 +0100

    day00/ Makefile: password in makefile's variable (for now) ex*/*.py: totally factorized

--- Day00-Data_engineer/ex04/items_table.py
+++ Day00-Data_engineer/ex04/items_table.py
@@ -1,9 +1,4 @@
-import os
-from pathlib import Path
-from get_psycopg_connection import get_psycopg_connection
-from create_table_if_not_exists import create_table_if_not_exists
-from import_csv_to_table import import_csv_to_table
-from get_all_csv_in_dir import get_all_csv_in_dir
+from create_table_import_all_csv_into import create_table_import_all_csv_into
 
 
 def main():
@@ -13,7 +8,6 @@ def main():
     """
 
     CONTAINER_CSV_DIR = "/data/item"
-    csv_dir = Path(CONTAINER_CSV_DIR).resolve()
     column_types = [
         "INT",
         "BIGINT",
@@ -21,46 +15,10 @@ def main():
         "VARCHAR(50)"
     ]
 
-    try:
-        connection = get_psycopg_connection()
-        cursor = connection.cursor()
-        print("Connected to the database successfully.")
-
-        for csv_file in get_all_csv_in_dir(csv_dir):
-            table_name = os.path.splitext(csv_file)[0]
-            csv_path = os.path.join(CONTAINER_CSV_DIR, csv_file)
-
-            with open(
-                os.path.join(csv_dir, csv_file),
-                "r",
-                encoding="utf-8"
-            ) as file:
-                headers = file.readline().strip().split(",")
-                print(f"Headers for {csv_file}: {headers}")
-
-            create_table_if_not_exists(
-                cursor,
-                table_name,
-                headers,
-                column_types
-            )
-            import_csv_to_table(
-                cursor,
-                table_name,
-                csv_path
-            )
-
-        connection.commit()
-        print("All CSV files have been imported successfully.")
-
-    except AssertionError as e:
-        print(f"{type(e).__name__}: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-    finally:
-        if "connection" in locals():
-            connection.close()
-            print("Database connection closed.")
+    create_table_import_all_csv_into(
+        CONTAINER_CSV_DIR,
+        column_types
+    )
 
 
 if __name__ == "__main__":
