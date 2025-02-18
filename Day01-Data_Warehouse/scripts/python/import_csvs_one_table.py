fe461d4399888610fb9501219239229c7140840b
Nociception <nicolas.stoutzer@hotmail.fr>
2025-02-18 20:19:49 +0100

    day00 and day01: ask after table report mentions the file involved in the IMPORT; day01/ex01: csv imported in chronological order

--- Day01-Data_Warehouse/scripts/python/import_csvs_one_table.py
+++ Day01-Data_Warehouse/scripts/python/import_csvs_one_table.py
@@ -3,6 +3,49 @@ from import_csv_to_table import import_csv_to_table
 from get_all_csv_in_dir import get_all_csv_in_dir
 from logger import logger
 from create_table import create_table
+import re
+
+
+def piscineds_csv_sort(csv_list: list[str]) -> list[str]:
+    """DOCSTRING"""
+
+    years = dict()
+    for csv_file in csv_list:
+        year = re.findall(r'\d+', csv_file)[0]
+        csv_file_sliced = csv_file[10:-4]
+        if year in years:
+            years[year].append(csv_file_sliced)
+        else:
+            years[year] = [csv_file_sliced]
+
+    MONTHS_ORDER = (
+        "jan",
+        "feb",
+        "mar",
+        "apr",
+        "may",
+        "jun",
+        "jul",
+        "aug",
+        "sep",
+        "oct",
+        "nov",
+        "dec"
+    )
+
+    for year, months_list in years.items():
+        sorted_months_list = []
+        for month in MONTHS_ORDER:
+            if month in months_list:
+                sorted_months_list.append(month)
+        years[year] = sorted_months_list
+
+    sorted_csv_list = []
+    for year, months in years.items():
+        for month in months:
+            sorted_csv_list.append(f"data_{year}_{month}.csv")
+
+    return sorted_csv_list
 
 
 def import_csvs_one_table(
@@ -14,8 +57,10 @@ def import_csvs_one_table(
 
     csv_dir = Path(container_csv_dir).resolve()
 
+
     try:
-        all_csv_in_dir = get_all_csv_in_dir(csv_dir)
+        all_csv_in_dir = piscineds_csv_sort(get_all_csv_in_dir(csv_dir))
+
         first_csv_file = all_csv_in_dir[0]
 
         with open(csv_dir / first_csv_file, "r", encoding="utf-8") as file:
